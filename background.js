f0399f358ae0ad770080b17938c881ce1ee4dddc
vincentsharp <vsharp@ucsd.edu>
2025-11-17 20:28:19 -0800

    Delete Old/ Add TruLens MVP OG

--- background.js
+++
@@ -1,8 +0,0 @@
-// background.js
-chrome.runtime.onMessage.addListener((msg, sender) => {
-  if (msg.type === 'TP_SELECT') {
-    // persist per-tab selection
-    chrome.storage.session.set({ ['tp:'+sender.tab.id]: msg.payload });
-    // optionally activate sidebar
-  }
-});
